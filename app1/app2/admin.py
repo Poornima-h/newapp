4deb0a37a234e8531ddb32817186fe39d566bcd3
Poornima-h <38782315+Poornima-h@users.noreply.github.com>
2021-08-06 02:50:59 -0700

    Add files via upload

---
+++ app1/app2/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+# Register your models here.
+from .models import Post2 ,Book2
+
+admin.site.register(Post2)
+admin.site.register(Book2)
